9259d246465c95690b916486e55c2e0c2fca225e
Jacob Palomo <jacob.palomo248@gmail.com>
2023-11-03 11:52:49 -0600

    Se agregan los tipos para typescript

---
+++ src/types/sommet-calendars.d.ts
@@ -0,0 +1,65 @@
+declare module 'sommet-calendars' {
+	export class Day {
+		private date: Date
+		private day: number
+		private month: number
+		private year: number
+		private weekday: number
+		private today: boolean
+		private weekdays: Array<string>
+		private mdWeekdays: Array<string>
+		private smWeekdays: Array<string>
+
+		constructor(date?: Date)
+
+		public getDay(): number
+		public getMonth(): number
+		public getYear(): number
+		public getWeekday(): number
+		public getWeekdayName(options?: { size: 'lg' | 'md' | 'sm' }): string
+		public toDate(): Date
+		public isToday(): boolean
+	}
+
+	export class Month {
+		private name: string
+		private month: number
+		private year: number
+		private calendar: Array<Day>
+		private monthDays: number
+		private names: Array<string>
+		private mdNames: Array<string>
+
+		private setCalendar(): Array<Day>
+		private setMonthDays(): number
+		private updateMonth(): void
+
+		constructor(year?: number, month?: number)
+
+		public getName(options?: { size: 'md' | 'lg' }): string
+		public nextMonth(): Month
+		public getNextMonth(): Month
+		public prevMonth(): Month
+		public getPrevMonth(): Month
+		public getMonth(): number
+		public getYear(): number
+		public getCalendar(): Array<Day>
+		public getMonthDays(): number
+	}
+
+	export class Year {
+		private year: number
+		private months: Array<Month>
+
+		private setMonths(): Array<Month>
+
+		constructor(year?: number)
+
+		public getYear(): number
+		public setYear(year?: number): Year
+		public getMonth(month?: number): Month
+		public nextYear(): Year
+		public prevYear(): Year
+		public getMonths(): Array<Month>
+	}
+}
